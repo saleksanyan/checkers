aa4ecfec10cba3054b25e747b8169948019f0c7f
saleksanyan <92786852+saleksanyan@users.noreply.github.com>
2022-05-11 17:09:20 +0400

    Class Pawn

--- Pawn.java
+++ Pawn.java
@@ -29,14 +29,10 @@ public class Pawn extends Piece{
 
 
     public static ArrayList<Position> reachablePositions(Board checkers, Position p) {
-//        ArrayList<Position> positions = new ArrayList<>(2);
-//        positions.addAll(canMove(checkers, p));
-//        jumpPositions(positions, checkers, p);
-//        return positions;
+
         ArrayList<Position> positions = new ArrayList<Position>(2);
         positions.addAll(canMove(checkers, p));
         ArrayList<Position> newPositions = new ArrayList<>();
-        //jumpPositions(positions, p, checkers);
         jumpPositions(positions, p, checkers, newPositions);
         return positions;
     }
@@ -68,27 +64,6 @@ public class Pawn extends Piece{
     }
 
     public static ArrayList<Position> eatable(Board checkers, Position p){
-//
-//        ArrayList<Position> positions = new ArrayList<>(2);
-//        int[] rank = {1, -1, 1, -1};
-//        int[] file = {1, 1, -1, -1};
-//        int rank1, file1;
-//        Position newPosition;
-//        for (int i = 0; i < 4; i++) {
-//            rank1 = p.getRank() + rank[i];
-//            file1 = p.getFile() + file[i];
-//            if(rank1 >= 0 && rank1 <= 7 && file1 >= 0 && file1 <= 7) {
-//                newPosition = new Position(rank1, file1);
-//
-//                if (!checkers.isEmpty(newPosition)){
-//                    if(checkers.getPieceAt(newPosition).getPieceColor() != checkers.getTurn()) {
-//                        positions.add(newPosition);
-//                    }
-//                }
-//            }
-//        }
-
-//        return positions;
 
         ArrayList<Position> positions = new ArrayList<>(2);
         int[] rank = {1, -1, 1, -1};
@@ -115,40 +90,37 @@ public class Pawn extends Piece{
 
     }
 
-//    public static void jumpPositions(ArrayList<Position> positions, Position position, Board checkers){
-//        ArrayList<Position> eatablePositions = eatable(checkers, position);
-//        Position pos;
-//
-//        for (Position p : eatablePositions) {
-//            int r = p.getRank() - position.getRank();
-//            int f = p.getFile() - position.getFile();
-//            int rank = p.getRank() + (r/(Math. abs(r)));
-//            int file = p.getFile() + (f/(Math. abs(f)));
-////            int rank = p.getRank() + (p.getRank() - position.getRank());
-////            int file = p.getFile() + (p.getFile() - position.getFile());
-//            if(rank >= 0 && rank <= 7 && file >= 0 && file <= 7) {
-//                pos = new Position(rank, file);
-//                boolean exists = false;
-//                for(Position p1: positions){
-//                    if(pos.equalsTo(p1)){
-//                        exists = true;
-//                        break;
-//                    }
-//                }
-//                if(exists)
-//                    continue;
-//
-//                if (!checkers.isEmpty(pos)) {
-//                    return;
-//                }
-//                positions.add(pos);
-//                jumpPositions(positions, pos, checkers);
-//            }
-//        }
-//    }
-//
-//
-//
+
+    public static void jumpPositions(ArrayList<Position> positions, Position position, Board board,
+                                     ArrayList<Position> newPositions) {
+        ArrayList<Position> eatablePositions = eatable(board, position);
+
+        Position pos;
+        for (Position p : eatablePositions) {
+            int r = p.getRank() - position.getRank();
+            int f = p.getFile() - position.getFile();
+            int rank = p.getRank() + (r/(Math. abs(r)));
+            int file = p.getFile() + (f/(Math. abs(f)));
+            if (rank >= 0 && rank <= 7 && file >= 0 && file <= 7) {
+                pos = new Position(rank, file);
+
+                if (!board.isEmpty(pos)) {
+                    continue;
+                }
+
+                for (Position p1 : newPositions) {
+                    if (pos.equalsTo(p1)) {
+                        return;
+                    }
+                }
+
+                newPositions.add(pos);
+                positions.add(pos);
+                jumpPositions(positions, pos, board, newPositions);
+            }
+        }
+    }
+
     public static String BecomeQueen(Board checkers, Position position) {
         if(checkers.getTurn().equals(Board.PieceColor.WHITE) && position.getRank() == 0){
             return "White";
@@ -157,35 +129,6 @@ public class Pawn extends Piece{
             return "Black";
         return "^_^";
     }
-public static void jumpPositions(ArrayList<Position> positions, Position position, Board board,
-                                 ArrayList<Position> newPositions) {
-    ArrayList<Position> eatablePositions = eatable(board, position);
-
-    Position pos;
-    for (Position p : eatablePositions) {
-        int r = p.getRank() - position.getRank();
-        int f = p.getFile() - position.getFile();
-        int rank = p.getRank() + (r/(Math. abs(r)));
-        int file = p.getFile() + (f/(Math. abs(f)));
-        if (rank >= 0 && rank <= 7 && file >= 0 && file <= 7) {
-            pos = new Position(rank, file);
-
-            if (!board.isEmpty(pos)) {
-                continue;
-            }
-
-            for (Position p1 : newPositions) {
-                if (pos.equalsTo(p1)) {
-                    return;
-                }
-            }
-
-            newPositions.add(pos);
-            positions.add(pos);
-            jumpPositions(positions, pos, board, newPositions);
-        }
-    }
-}
 
 }
 
